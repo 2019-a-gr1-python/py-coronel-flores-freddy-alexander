7aa550409913de29cd9d2da471866d03567d390a
alexc957 <alexcoronel1995@gmail.com>
2019-06-08 10:42:04 -0500

    dataframe filtering and read multiple jsons in a directory

--- 03_Pandas/D_lectura_json.py
+++ 03_Pandas/D_lectura_json.py
@@ -12,20 +12,55 @@ import os
 
 path = '/home/alex/Documents/py-coronel-flores-freddy-alexander/03_Pandas/data/artwork/'
 archivo = 'a/000/a00001-1035.json'
-path_archivo = path+archivo
-llaves = ['id','all_artists','title','medium','dateText','acquisitionYear',
-          'wid','height','units']
-registros = []
-with open(path_archivo) as txt_json:
-    contenido_json = json.load(txt_json)
-    
-    print(contenido_json)
-    print(type(contenido_json))
-    registros_df = []
+archivo = '/a/000/a00001-1035.json'
+
+path_archivo = path + archivo
+
+llaves = ['id','all_artists',
+          'title','medium',
+          'dateText','acquisitionYear',
+          'height','width','units']
+
+
+def leer_json(path_archivo,llaves):
+    with open(path_archivo) as texto_json:
+        contenido_json = json.load(texto_json)
+    registro_df_lista = []
     for llave in llaves:
         valor = contenido_json[llave]
-        registros_df.append(llave)
-    serie = tuple(registros_df)
+        registro_df_lista.append(valor)
+    return registro_df_lista
+
+#leer_json(path_archivo, llaves)
+
+def leer_json_en_carpertas(directorio,llaves):
+    trabajos_arte = []
+    print(type(os.walk(directorio)))
+    for path_raiz, lista_directorios, archivos in os.walk(directorio):
+        #print(path_raiz)
+        #print(lista_directorios)
+        #print(archivos)
+        for nombre_archivo in archivos:
+            if nombre_archivo.endswith('json'):
+                directorio_archivo = os.path.join(
+                    path_raiz,
+                    nombre_archivo
+                )
+            pieza_arte = leer_json(directorio_archivo,llaves)
+            trabajos_arte.append(pieza_arte)
+            break
+    df = pd.DataFrame.from_records(
+        trabajos_arte,
+        columns = llaves,
+        index = "id"
+        
+    )
+    print(df)
+    print(df.shape)
+leer_json_en_carpertas(path,llaves)
+
     
-    print(serie)
     
+
+
+
